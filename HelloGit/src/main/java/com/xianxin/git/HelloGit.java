6cb0510914aa576e040a716c002294403c31b815
ggokayboom <1282208632@qq.com>
2021-02-27 00:55:28 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/xianxin/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.xianxin.git;
+
+/**
+ * @author: xianxin shao
+ * @date 2021/2/27 0:39
+ * @email: 1282208632@qq.com
+ * @Tel: 13958697970
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
